f4e94eed07da6aa2f44406e49347bbb2e3c36fec
Matt Driscoll <mdriscoll@esri.com>
2017-07-05 20:14:53 -0700

    add starting point folder

--- demos/magnifier-start/app/Magnifier/nls/Magnifier.js
+++ demos/magnifier-start/app/Magnifier/nls/Magnifier.js
@@ -1,7 +1,5 @@
 define({
   root: ({
-    dragHelp: "Drag magnifying glass.",
-    keyboardHelp: "Use the arrow keys to move the magnifying glass."
   }),
   "es": 1
 });
--- demos/magnifier-start/app/Magnifier/nls/es/Magnifier.js
+++ demos/magnifier-start/app/Magnifier/nls/es/Magnifier.js
@@ -1,4 +1,2 @@
 define({
-  dragHelp: "Arrastre la lupa.",
-  keyboardHelp: "Utilice las teclas de flecha para mover la lupa."
 });
